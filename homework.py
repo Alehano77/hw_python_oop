70731ed9326814982994f9d009946c2702185d11
Alehano77 <120413786+Alehano77@users.noreply.github.com>
2022-12-30 17:33:45 +0300

    Update homework.py

--- homework.py
+++ homework.py
@@ -149,7 +149,7 @@ class Swimming(Training):
 
     def get_spent_calories(self) -> float:
         spent_calories_swm: str = ((self.get_mean_speed() + self.FRST_COEF)
-                                   * self.SEC_COEF * self.weight 
+                                   * self.SEC_COEF * self.weight
                                    * self.duration)
         return spent_calories_swm
 
